51bbbd4539ae55997166c843cd89c9560f64f4f3
JuneMbuvi <mbuvimbatha@gmail.com>
2023-03-16 10:47:22 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - main function
  * Return: always 0
@@ -7,5 +8,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);	
+	return (0);
 }
