aa8fceb5be9bf75182a8b17dfb420d0aec01b7bf
JuneMbuvi <mbuvimbatha@gmail.com>
2023-03-27 18:32:12 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes an integer and updates its value to 98
+ * @n: parameter being tested
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
