0414bda44404f8c637fe4ade14562ef78e312616
JuneMbuvi <mbuvimbatha@gmail.com>
2023-03-30 10:39:59 -0700

    look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - chnages lowercase to uppercase
+ * Return: n
+ * @n: parameter being tested
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
