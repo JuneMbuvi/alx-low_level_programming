d012bce10976f70c9fb8cc8cd3e082459330044f
JuneMbuvi <mbuvimbatha@gmail.com>
2023-06-20 10:25:22 -0700

    creating structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,11 +11,12 @@
  *
  * Description: Longer description
  */
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}my_dog;
+} my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 /**
