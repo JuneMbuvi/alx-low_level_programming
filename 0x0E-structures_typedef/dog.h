6111938c09ca01af31d9cc3646ee67ff215ccaa6
JuneMbuvi <mbuvimbatha@gmail.com>
2023-06-19 19:13:58 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,8 @@ struct dog {
 }my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - new type for dog struct
+ */
+typedef struct dog dog_t;
 #endif
