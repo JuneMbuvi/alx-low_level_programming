42f8caa2b893aee0d9a2ec8c952e62aca136cb44
JuneMbuvi <mbuvimbatha@gmail.com>
2023-06-21 20:22:41 -0700

    simple calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "calc.h"
+#include "3-calc.h"
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: pointer of type char
@@ -10,16 +10,17 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i;
 
-	if (s != "+" || s != "-" || s != "*"
-			|| s != "/" || s != "%")
-		return (NULL);
+	i = 0;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i].f);
 }
