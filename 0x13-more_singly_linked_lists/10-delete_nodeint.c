d7ab0297fd290d29206fb1b53e7df38cb43a6cb3
JuneMbuvi <mbuvimbatha@gmail.com>
2023-07-04 19:28:10 -0700

    deleting at an idx

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	listint_t *previous = *head, *current = *head;
 
-	if (head == NULL)
+	if (*head == NULL || !head)
 		return (-1);
 	else if (index == 0)
 	{
