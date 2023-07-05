35354ae164edfccc3b1fd80894611dc55b285ae7
JuneMbuvi <mbuvimbatha@gmail.com>
2023-07-04 18:59:16 -0700

    inserting at an idx

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,5 +3,5 @@
 #include <stdlib.h>
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-
+	listint_t *previouscurrent
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,30 +6,37 @@
  * @head: pointer to head pointer
  * @idx: index of the list where the new node should be added
  * @n: data in the node
- * Return: ptr2
+ * Return: ptr2 if successful
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr = *head, *ptr2;
+	unsigned int i = 0;
 
 	ptr2 = malloc(sizeof(listint_t));
-	if (!ptr2 || idx < 1)
+
+	if (!ptr2 || *head == NULL)
 		return (NULL);
 	ptr2->n = n;
 	ptr2->next = NULL;
 
-	else if (idx == 1)
+	if (idx == 0)
 	{
-
+		ptr2->next = (*head);
+		(*head) = ptr2;
+		return (ptr2);
 	}
-	idx--;
-	while (idx)
+
+	for (i = 0; ptr && i < idx; i++)
 	{
-		ptr = ptr->next;
-		idx--;
+		if (i == idx - 1)
+		{
+			ptr2->next = ptr->next;
+			ptr->next = ptr2;
+			return (ptr2);
+		}
+		else
+			ptr = ptr->next;
 	}
-	ptr2->next = ptr->next;
-	ptr->next = ptr2;
-
-	return (ptr2);
+	return (NULL);
 }
