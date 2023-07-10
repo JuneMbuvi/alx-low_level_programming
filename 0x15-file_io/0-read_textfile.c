fd0226069059dba7d2da8a554ed38883a6fdd916
JuneMbuvi <mbuvimbatha@gmail.com>
2023-07-10 10:04:49 -0700

    read only

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <unistd.h>
 #include <fcntl.h>
-
+#include <unistd.h>
 /**
- * read_textfile - text file and prints it to POSIX
- * @filename: name of file
+ * read_textfile - text file and prints it to the POSIX standard output
+ * @filename: ptr to name of file to read from
  * @letters: number of letters it should read and print
- * Return: actual number of letters it could read and print
+ * Return: no. of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t m, n;
 	ssize_t fd;
 	char *buf;
-	ssize_t r;
-	ssize_t w;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	if (filename == NULL)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	r = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, r);
-	if (w == -1)
-		return (0);
 
+	buf = malloc(sizeof(char ) * letters);
+
+	m = read(fd, buf, letters);
+	n = write(STDOUT_FILENO, buf, m);
+
+	free(buf);
 	close(fd);
-	return (w);
+	return (n);
 }
