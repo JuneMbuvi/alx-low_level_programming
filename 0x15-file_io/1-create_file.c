5896922768a9ab5fd823ad13f47ee571d1e45b1f
JuneMbuvi <mbuvimbatha@gmail.com>
2023-07-10 11:51:08 -0700

    creating files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,17 +11,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0, i = 0;
+	int fd, len = 0, m;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-	if (fd == -1)
+	if (!filename)
 		return (-1);
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		i++;
-		write(STDIN_FILENO, text_content, len);
-		len++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	m = write(fd, text_content, len);
+
+	if (fd == -1 || m == -1)
+		return (-1);
+
+	close(fd);
 	return (1);
 }
