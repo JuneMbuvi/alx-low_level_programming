fc8c8ca61d2ff864b3273c62773608b556d38c19
JuneMbuvi <mbuvimbatha@gmail.com>
2023-08-21 08:33:22 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/strncat.c
@@ -0,0 +1,25 @@
+/**
+ * *_strncat - concatenates 2 strings
+ * Return: dest
+ * @dest: first pointer
+ * @src: second pointer
+ * @n: parameter being tested
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (dest[x] != '\0')
+		x++;
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
